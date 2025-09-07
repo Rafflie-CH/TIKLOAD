0665dba8273db61d5fda66f28ca9bd428888d982
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-07 08:45:28 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}"
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        fadeIn: "fadeIn 0.5s ease-in-out"
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
-        }
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
