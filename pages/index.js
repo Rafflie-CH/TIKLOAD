cf5d6835218f5246d283f85cd515bb085d0ec049
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-07 16:03:43 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
       const data = await res.json();
 
       if (!data.success) {
-        alert("Gagal mengambil data");
+        alert("Gagal mengambil data pastikan url sudah benar");
         return;
       }
       setResult(data);
